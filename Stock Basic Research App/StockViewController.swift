a66c1a9941c744a5964074073932a93a5e082ac5
Isaac Kit Hong Ng <isaacngkh@gmail.com>
2019-05-22 17:32:00 -0700

    Done with toggle button in stock page, and adding/deleteing symbol from core data

--- Stock Basic Research App/StockViewController.swift
+++ Stock Basic Research App/StockViewController.swift
@@ -7,16 +7,91 @@
 //
 
 import UIKit
+import CoreData
 
 class StockViewController: UIViewController {
 
     var sym: String = ""
+    let favoriteEntity = "Favorite"
+    
+    @IBOutlet weak var favoriteButton: UIBarButtonItem!
+    let toggleEditSelector = #selector(favoriteButtonTap)
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let context = appDelegate.persistentContainer.viewContext
+        
+        // change button if this stock is favorited
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: favoriteEntity)
+        fetchRequest.predicate = NSPredicate(format: "symbol = %@", sym)
+        
+        do {
+            let test = try context.fetch(fetchRequest)
+            if test.count > 0 {
+                let stopBarButtonItem = UIBarButtonItem(barButtonSystemItem: .stop, target: self, action: toggleEditSelector)
+                navigationItem.rightBarButtonItem = stopBarButtonItem
+                setEditing(true, animated: true)
+            }
+        } catch {
+            fatalError("Error in StockViewController: Failed fetch while loading page.")
+        }
+        
     }
+    
+    @IBAction func favoriteButtonTap(_ sender: UIBarButtonItem) {
+        guard let systemItem = sender.value(forKey: "systemItem") as? Int else {
+            fatalError("Error in StockViewController: can't find systemItem")
+        }
+        
+        let context = appDelegate.persistentContainer.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: favoriteEntity, in: context)!
+
+        // switch between add and stop button, and add/remove stock symbol from favorite accordingly
+        
+        switch systemItem {
+        case UIBarButtonItem.SystemItem.add.rawValue:
+            let stopBarButtonItem = UIBarButtonItem(barButtonSystemItem: .stop, target: self, action: toggleEditSelector)
+            navigationItem.rightBarButtonItem = stopBarButtonItem
+            setEditing(true, animated: true)
+            
+            // add to core data
+            let stock = NSManagedObject(entity: entity, insertInto: context)
+            stock.setValue(sym, forKey: "symbol")
+            stock.setValue("TODO in StockViewController", forKey: "name")
+            stock.setValue(0, forKey: "lastPrice")
+            
+        case UIBarButtonItem.SystemItem.stop.rawValue:
+            let addBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: toggleEditSelector)
+            navigationItem.rightBarButtonItem = addBarButtonItem
+            setEditing(false, animated: true)
+            
+            // remove from core data
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: favoriteEntity)
+            fetchRequest.predicate = NSPredicate(format: "symbol = %@", sym)
+            
+            do {
+                let test = try context.fetch(fetchRequest)
+                let objectToDelete = test[0] as! NSManagedObject
+                context.delete(objectToDelete)
+            } catch {
+                fatalError("Error in StockViewController: Failed fetch while deleting.")
+            }
+            
+        default:
+            fatalError("Error in StockViewController: UIBarButtonItem ststem item not recognized")
+        }
+        
+        do {
+            try context.save()
+        } catch let error as NSError {
+            fatalError("Could not save. \(error), \(error.userInfo)")
+        }
+    }
+    
     
 
     /*
