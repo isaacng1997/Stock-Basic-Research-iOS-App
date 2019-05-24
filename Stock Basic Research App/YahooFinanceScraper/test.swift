f0c944283ad7c6eeaaf1f614c63da4a3b3d4c194
Isaac Kit Hong Ng <isaacngkh@gmail.com>
2019-05-23 23:21:00 -0700

    Scrap more data off statistic page

--- Stock Basic Research App/YahooFinanceScraper/test.swift
+++ Stock Basic Research App/YahooFinanceScraper/test.swift
@@ -18,6 +18,15 @@ struct Company {
     var enterpriseValue: String
     var trailingPE: String
     var forwardPE: String
+    var pegRatio: String
+    var priceSale: String
+    var priceBook: String
+    var enterpriseValueRevenue: String
+    var enterpriseValueEBITDA: String
+    var profitMargin: String
+    var operativeMargin: String
+    var returnOnAssets: String
+    var returnOnEquity: String
 }
 
 var company_dictionary:[String:Company] = [:]
@@ -70,6 +79,25 @@ func historical_url(symbol: String,
                           interval: interval)
 }
 
+func scrap_from_statistics(htmlString:String, left:Int, right:Int, name_of_stat:String) -> String {
+    let leftString = """
+    <td class="Fz(s) Fw(500) Ta(end)" data-reactid="\(left)">
+    """
+    let rightString = """
+    </td></tr><tr data-reactid="\(right)">
+    """
+    guard let leftSideRange = htmlString.range(of: leftString) else {
+        print("couldn't find left range when parsing for " + name_of_stat)
+        return ""
+    }
+    guard let rightSideRange = htmlString.range(of: rightString) else {
+        print("couldn't find right range when parsing for " + name_of_stat)
+        return ""
+    }
+    let rangeOfTheData = leftSideRange.upperBound..<rightSideRange.lowerBound
+    return String(htmlString[rangeOfTheData])
+}
+
 func get_basic_info(symbol: String) {
     let url = URL(string: baseURL + symbol + statURL + symbol)!
     
@@ -105,97 +133,91 @@ func get_basic_info(symbol: String) {
         var rangeOfTheData = leftSideRange!.upperBound..<rightSideRange!.lowerBound
         let name = String(htmlString[rangeOfTheData])
         
+        let marketCap = scrap_from_statistics(htmlString: htmlString, left: 19, right: 20, name_of_stat: "marketCap")
+        let enterpriseValue = scrap_from_statistics(htmlString: htmlString, left: 26, right: 27, name_of_stat: "enterpriseValue")
+        let trailingPE = scrap_from_statistics(htmlString: htmlString, left: 33, right: 34, name_of_stat: "trailingPE")
+        let forwardPE = scrap_from_statistics(htmlString: htmlString, left: 40, right: 41, name_of_stat: "forwardPE")
+        let pegRatio = scrap_from_statistics(htmlString: htmlString, left: 47, right: 48, name_of_stat: "pegRatio")
+        let priceSale = scrap_from_statistics(htmlString: htmlString, left: 54, right: 55, name_of_stat: "priceSale")
+        let priceBook = scrap_from_statistics(htmlString: htmlString, left: 61, right: 62, name_of_stat: "priceBook")
+        let enterpriseValueRevenue = scrap_from_statistics(htmlString: htmlString, left: 68, right: 69, name_of_stat: "enterpriseValueRevenue")
         
-        // parse for market cap
         leftString = """
-                    <td class="Fz(s) Fw(500) Ta(end)" data-reactid="19">
-                    """
-        rightString = """
-                    </td></tr><tr data-reactid="20">
-                    """
-        leftSideRange = htmlString.range(of: leftString)
-        rightSideRange = htmlString.range(of: rightString)
-        if leftSideRange == nil {
-            print("couldn't find left range when parsing for market cap")
-            return
-        }
-        if rightSideRange == nil {
-            print("couldn't find right range when parsing for market cap")
-            return
-        }
-        rangeOfTheData = leftSideRange!.upperBound..<rightSideRange!.lowerBound
-        let marketCap = String(htmlString[rangeOfTheData])
-        
-        
-        // parse for enterprise value
-        leftString = """
-        <td class="Fz(s) Fw(500) Ta(end)" data-reactid="26">
+        <td class="Fz(s) Fw(500) Ta(end)" data-reactid="75">
         """
         rightString = """
-        </td></tr><tr data-reactid="27">
+        </td></tr></tbody></table></div></div><div class="" data-reactid="76">
         """
         leftSideRange = htmlString.range(of: leftString)
         rightSideRange = htmlString.range(of: rightString)
         if leftSideRange == nil {
-            print("couldn't find left range when parsing for enterprise value")
+            print("couldn't find left range when parsing for name of enterpriseValueEBITDA")
             return
         }
         if rightSideRange == nil {
-            print("couldn't find right range when parsing for enterprise value")
+            print("couldn't find right range when parsing for name of enterpriseValueEBITDA")
             return
         }
         rangeOfTheData = leftSideRange!.upperBound..<rightSideRange!.lowerBound
-        let enterpriseValue = String(htmlString[rangeOfTheData])
+        let enterpriseValueEBITDA = String(htmlString[rangeOfTheData])
         
+        let profitMargin = scrap_from_statistics(htmlString: htmlString, left: 112, right: 113, name_of_stat: "profitMargin")
         
-        // parse for trailingPE
         leftString = """
-        <td class="Fz(s) Fw(500) Ta(end)" data-reactid="33">
+        <td class="Fz(s) Fw(500) Ta(end)" data-reactid="119">
         """
         rightString = """
-        </td></tr><tr data-reactid="34">
+        </td></tr></tbody></table></div><div data-reactid="120">
         """
         leftSideRange = htmlString.range(of: leftString)
         rightSideRange = htmlString.range(of: rightString)
         if leftSideRange == nil {
-            print("couldn't find left range when parsing for trailingPE")
+            print("couldn't find left range when parsing for name of operativeMargin")
             return
         }
         if rightSideRange == nil {
-            print("couldn't find right range when parsing for trailingPE")
+            print("couldn't find right range when parsing for name of operativeMargin")
             return
         }
         rangeOfTheData = leftSideRange!.upperBound..<rightSideRange!.lowerBound
-        let trailingPE = String(htmlString[rangeOfTheData])
+        let operativeMargin = String(htmlString[rangeOfTheData])
         
+        let returnOnAssets = scrap_from_statistics(htmlString: htmlString, left: 131, right: 132, name_of_stat: "profitMargin")
         
-        // parse for forwardPE
         leftString = """
-        <td class="Fz(s) Fw(500) Ta(end)" data-reactid="40">
+        <td class="Fz(s) Fw(500) Ta(end)" data-reactid="138">
         """
         rightString = """
-        </td></tr><tr data-reactid="41">
+        </td></tr></tbody></table></div><div data-reactid="139">
         """
         leftSideRange = htmlString.range(of: leftString)
         rightSideRange = htmlString.range(of: rightString)
         if leftSideRange == nil {
-            print("couldn't find left range when parsing for forwardPE")
+            print("couldn't find left range when parsing for name of returnOnEquity")
             return
         }
         if rightSideRange == nil {
-            print("couldn't find right range when parsing for forwardPE")
+            print("couldn't find right range when parsing for name of returnOnEquity")
             return
         }
         rangeOfTheData = leftSideRange!.upperBound..<rightSideRange!.lowerBound
-        let forwardPE = String(htmlString[rangeOfTheData])
-        
+        let returnOnEquity = String(htmlString[rangeOfTheData])
         
         let c = Company(symbol: symbol,
                         name: name,
                         marketCap: marketCap,
                         enterpriseValue: enterpriseValue,
                         trailingPE: trailingPE,
-                        forwardPE: forwardPE)
+                        forwardPE: forwardPE,
+                        pegRatio: pegRatio,
+                        priceSale: priceSale,
+                        priceBook: priceBook,
+                        enterpriseValueRevenue: enterpriseValueRevenue,
+                        enterpriseValueEBITDA: enterpriseValueEBITDA,
+                        profitMargin: profitMargin,
+                        operativeMargin: operativeMargin,
+                        returnOnAssets: returnOnAssets,
+                        returnOnEquity: returnOnEquity)
         company_dictionary[symbol] = c
         
         print(company_dictionary)
@@ -208,4 +230,4 @@ func get_basic_info(symbol: String) {
 
 get_basic_info(symbol: "AAPL")
 get_basic_info(symbol: "AMZN")
-
+get_basic_info(symbol: "GOOGL")
